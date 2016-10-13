55bee8a59edbb0676d8b371c2ceff7fc02fbd105
Stephanie Martinez <stephanie@hawkbane.net>
2016-10-12 21:28:04 -0500

    Added Required SignalR Classes

---
+++ SAMS/MyHub.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using System.Web;
+
+
+namespace SAMS
+{
+    public static class groupNames
+    {
+        public const string Monitor = "Monitor";
+        public const string Sasha = "Sasha";
+    }
+
+    public class MyHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
---
+++ SAMS/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.AspNet.SignalR;
+using System.Data.Entity;
+
+
+[assembly: OwinStartup(typeof(SAMS.Startup))]
+
+namespace SAMS
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HubConfiguration hubConfiguration = new HubConfiguration();
+            hubConfiguration.EnableDetailedErrors = true;
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR(hubConfiguration);
+            GlobalHost.Configuration.MaxIncomingWebSocketMessageSize = null;
+
+            /* Set the following tables to an initialized state at startup
+
+                sashaSessions (empty)
+                chatSessions (empty)
+                chatHelpers (connectionStatus to notConnected)
+            Database.InitializeTables();
+            */
+        }
+    }
+}
